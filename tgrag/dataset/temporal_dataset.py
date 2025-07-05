94e6bc59c6a50f808d3ea6f2a769064d4a22a390
sebastian9991 <sebastian.sabry1@gmail.com>
2025-07-05 15:47:46 -0400

    temporal dataset supporting split on transductive nodes only for cr_score

--- tgrag/dataset/temporal_dataset.py
+++ tgrag/dataset/temporal_dataset.py
@@ -50,7 +50,10 @@ class TemporalDataset(InMemoryDataset):
 
         df = pd.read_csv(node_path)
         df = df.set_index('node_id').loc[mapping.keys()]
-        cr_score = torch.tensor(df['cr_score'].values, dtype=torch.float)
+        labeled_mask = df['cr_score'].notna().values
+        cr_score = torch.tensor(
+            df['cr_score'].fillna(-1).values, dtype=torch.float
+        ).unsqueeze(1)
 
         edge_index, edge_attr = load_edge_csv(
             path=edge_path, src_index_col='src', dst_index_col='dst', encoders=None
@@ -58,17 +61,21 @@ class TemporalDataset(InMemoryDataset):
 
         adj_t = to_torch_csr_tensor(edge_index, size=(x_full.size(0), x_full.size(0)))
 
-        cr_score = cr_score.unsqueeze(1)
-
         data = Data(x=x_full, y=cr_score, edge_index=edge_index, edge_attr=edge_attr)
         data.adj_t = adj_t
+
+        data.labeled_mask = torch.tensor(labeled_mask, dtype=torch.bool)
 
         torch.save(self.collate([data]), self.processed_paths[0])
 
     def get_idx_split(self) -> Dict:
-        n = self[0].num_nodes
+        data = self[0]
+        labeled_idx = data.labeled_mask.nonzero(as_tuple=True)[0]
+        n = labeled_idx.size(0)
+        train_end = int(0.6 * n)
+        valid_end = int(0.8 * n)
         return {
-            'train': torch.arange(0, int(0.6 * n)),
-            'valid': torch.arange(int(0.6 * n), int(0.8 * n)),
-            'test': torch.arange(int(0.8 * n), n),
+            'train': labeled_idx[:train_end],
+            'valid': labeled_idx[train_end:valid_end],
+            'test': labeled_idx[valid_end:],
         }
