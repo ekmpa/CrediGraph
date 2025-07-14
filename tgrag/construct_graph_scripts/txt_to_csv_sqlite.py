fc107339f88fc95adb7f23f72c9f87c8996fee66
sebastian9991 <sebastian.sabry1@gmail.com>
2025-07-14 13:36:30 -0400

    fix: bug where dst nodes pr/hc values were -1

--- tgrag/construct_graph_scripts/txt_to_csv_sqlite.py
+++ tgrag/construct_graph_scripts/txt_to_csv_sqlite.py
@@ -158,114 +158,124 @@ def merge_vertices_rank_centrality(folder_path: str) -> None:
     nodes_out_path = os.path.join(folder_path, 'temporal_nodes.csv')
     edges_out_path = os.path.join(folder_path, 'temporal_edges.csv')
 
-    with (
-        open(nodes_out_path, 'w', newline='') as nodes_out,
-        open(edges_out_path, 'w', newline='') as edges_out,
-    ):
-        node_writer = csv.writer(nodes_out)
-        edge_writer = csv.writer(edges_out)
+    if not os.path.exists(nodes_out_path):
+        with (
+            open(nodes_out_path, 'w', newline='') as nodes_out,
+        ):
+            node_writer = csv.writer(nodes_out)
 
-        node_writer.writerow(['domain', 'node_id', 'time_id', 'pr_val', 'hc_val'])
-        edge_writer.writerow(
-            [
-                'src',
-                'dst',
-                'time_id',
-                'pr_val_src',
-                'hc_val_src',
-                'pr_val_dst',
-                'hc_val_dst',
-            ]
-        )
+            node_writer.writerow(['domain', 'node_id', 'time_id', 'pr_val', 'hc_val'])
+            for vfile in vertex_files:
+                time_id = extract_time_id(os.path.basename(vfile))
+                db_rank_path = time_id_to_db_rank.get(cast(str, time_id), '')
 
-        for vfile in vertex_files:
-            time_id = extract_time_id(os.path.basename(vfile))
-            db_rank_path = time_id_to_db_rank.get(cast(str, time_id), '')
+                logging.info(f'Processing vertex file: {os.path.basename(vfile)}')
+                total_lines = count_lines(vfile)
 
-            logging.info(f'Processing vertex file: {os.path.basename(vfile)}')
-            total_lines = count_lines(vfile)
+                conn = sqlite3.connect(db_rank_path)
+                conn.execute('PRAGMA journal_mode=OFF')
+                conn.execute('PRAGMA synchronous=OFF')
+                conn.execute('PRAGMA temp_store=MEMORY')
+                cur = conn.cursor()
 
-            conn = sqlite3.connect(db_rank_path)
-            conn.execute('PRAGMA journal_mode=OFF')
-            conn.execute('PRAGMA synchronous=OFF')
-            conn.execute('PRAGMA temp_store=MEMORY')
-            cur = conn.cursor()
+                with open(vfile, 'r') as vf:
+                    for line in tqdm(
+                        vf, total=total_lines, desc='Writing nodes', unit='lines'
+                    ):
+                        parts = line.strip().split('\t')
+                        if len(parts) < 2:
+                            continue
+                        node_id, domain = parts[0], parts[1]
+                        cur.execute(
+                            'SELECT pr_val, hc_val FROM ranks WHERE domain = ?',
+                            (domain,),
+                        )
+                        result = cur.fetchone()
+                        pr_val = result[0] if result else -1.0
+                        hc_val = result[1] if result else -1.0
+                        node_writer.writerow([domain, node_id, time_id, pr_val, hc_val])
 
-            with open(vfile, 'r') as vf:
-                for line in tqdm(
-                    vf, total=total_lines, desc='Writing nodes', unit='lines'
-                ):
-                    parts = line.strip().split('\t')
-                    if len(parts) < 2:
-                        continue
-                    node_id, domain = parts[0], parts[1]
-                    cur.execute(
-                        'SELECT pr_val, hc_val FROM ranks WHERE domain = ?', (domain,)
-                    )
-                    result = cur.fetchone()
-                    pr_val = result[0] if result else -1.0
-                    hc_val = result[1] if result else -1.0
-                    node_writer.writerow([domain, node_id, time_id, pr_val, hc_val])
+                conn.close()
+    else:
+        logging.info('Skipping node CSV construction: file already exists.')
 
-            conn.close()
+    if not os.path.exists(edges_out_path):
+        with (
+            open(edges_out_path, 'w', newline='') as edges_out,
+        ):
+            edge_writer = csv.writer(edges_out)
 
-        for efile in edge_files:
-            time_id = extract_time_id(os.path.basename(efile))
-            if not time_id:
-                continue
-            db_rank_path = time_id_to_db_rank.get(time_id, '')
-            db_node_path = time_id_to_db_node.get(time_id, '')
-            logging.info(f'Processing edge file: {os.path.basename(efile)}')
-            total_lines = count_lines(efile)
+            edge_writer.writerow(
+                [
+                    'src',
+                    'dst',
+                    'time_id',
+                    'pr_val_src',
+                    'hc_val_src',
+                    'pr_val_dst',
+                    'hc_val_dst',
+                ]
+            )
 
-            conn_rank = sqlite3.connect(db_rank_path)
-            conn_rank.execute('PRAGMA journal_mode=OFF')
-            conn_rank.execute('PRAGMA synchronous=OFF')
-            conn_rank.execute('PRAGMA temp_store=MEMORY')
-            cur_rank = conn_rank.cursor()
+            for efile in edge_files:
+                time_id = extract_time_id(os.path.basename(efile))
+                if not time_id:
+                    continue
+                db_rank_path = time_id_to_db_rank.get(time_id, '')
+                db_node_path = time_id_to_db_node.get(time_id, '')
+                logging.info(f'Processing edge file: {os.path.basename(efile)}')
+                total_lines = count_lines(efile)
 
-            domain_to_rank = {}
-            cur_rank.execute('SELECT domain, pr_val, hc_val from ranks')
-            for domain, pr, hc in cur_rank.fetchall():
-                domain_to_rank[domain] = (pr, hc)
+                conn_rank = sqlite3.connect(db_rank_path)
+                conn_rank.execute('PRAGMA journal_mode=OFF')
+                conn_rank.execute('PRAGMA synchronous=OFF')
+                conn_rank.execute('PRAGMA temp_store=MEMORY')
+                cur_rank = conn_rank.cursor()
 
-            conn_node = sqlite3.connect(db_node_path)
-            conn_node.execute('PRAGMA journal_mode=OFF')
-            conn_node.execute('PRAGMA synchronous=OFF')
-            conn_node.execute('PRAGMA temp_store=MEMORY')
-            cur_node = conn_node.cursor()
+                domain_to_rank = {}
+                cur_rank.execute('SELECT domain, pr_val, hc_val from ranks')
+                for domain, pr, hc in cur_rank.fetchall():
+                    domain_to_rank[domain] = (pr, hc)
 
-            with open(efile, 'r') as ef:
-                for line in tqdm(
-                    ef, total=total_lines, desc='Writing edges', unit='lines'
-                ):
-                    parts = line.strip().split('\t')
-                    if len(parts) < 2:
-                        continue
-                    src, dst = parts[0], parts[1]
-                    cur_node.execute(
-                        'SELECT domain FROM node_map WHERE node_id = ?', (src,)
-                    )
-                    src_domain = cur_node.fetchone()
-                    pr_src, hc_src = (
-                        domain_to_rank.get(src_domain[0], (-1.0, -1.0))
-                        if src_domain
-                        else (-1.0, -1.0)
-                    )
+                conn_node = sqlite3.connect(db_node_path)
+                conn_node.execute('PRAGMA journal_mode=OFF')
+                conn_node.execute('PRAGMA synchronous=OFF')
+                conn_node.execute('PRAGMA temp_store=MEMORY')
+                cur_node = conn_node.cursor()
 
-                    cur_node.execute(
-                        'SELECT domain FROM node_map WHERE node_id = ?', (dst,)
-                    )
-                    dst_domain = cur_rank.fetchone()
-                    pr_dst, hc_dst = (
-                        domain_to_rank.get(dst_domain[0], (-1.0, -1.0))
-                        if dst_domain
-                        else (-1.0, -1.0)
-                    )
-                    edge_writer.writerow(
-                        [src, dst, time_id, pr_src, hc_src, pr_dst, hc_dst]
-                    )
-            cur_rank.close()
-            cur_node.close()
+                with open(efile, 'r') as ef:
+                    for line in tqdm(
+                        ef, total=total_lines, desc='Writing edges', unit='lines'
+                    ):
+                        parts = line.strip().split('\t')
+                        if len(parts) < 2:
+                            continue
+                        src, dst = parts[0], parts[1]
+                        cur_node.execute(
+                            'SELECT domain FROM node_map WHERE node_id = ?', (src,)
+                        )
+                        src_domain = cur_node.fetchone()
+                        pr_src, hc_src = (
+                            domain_to_rank.get(src_domain[0], (-1.0, -1.0))
+                            if src_domain
+                            else (-1.0, -1.0)
+                        )
+
+                        cur_node.execute(
+                            'SELECT domain FROM node_map WHERE node_id = ?', (dst,)
+                        )
+                        dst_domain = cur_node.fetchone()
+                        pr_dst, hc_dst = (
+                            domain_to_rank.get(dst_domain[0], (-1.0, -1.0))
+                            if dst_domain
+                            else (-1.0, -1.0)
+                        )
+                        edge_writer.writerow(
+                            [src, dst, time_id, pr_src, hc_src, pr_dst, hc_dst]
+                        )
+                cur_rank.close()
+                cur_node.close()
+    else:
+        logging.info('Skipping edge CSV construction: file already exists.')
 
     logging.info('Done.')
