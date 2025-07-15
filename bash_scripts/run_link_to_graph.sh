f0d0d2c32a1a974e15c678bbb8f4ab14cf385c6a
ekmpa <emmakndp@gmail.com>
2025-07-15 11:54:57 -0400

    typo in submit

--- bash_scripts/run_link_to_graph.sh
+++ bash_scripts/run_link_to_graph.sh
@@ -49,7 +49,7 @@ rm -rf "$PROJECT_ROOT/bash_scripts/spark-warehouse/host_graph_output_edges"
   --executor-memory 4g \
   --conf spark.sql.shuffle.partitions=512 \
   --conf spark.io.compression.codec=snappy \
-  --conf spark.default.parallelism=512
+  --conf spark.default.parallelism=512 \
   --py-files "$PROJECT_ROOT/tgrag/cc-scripts/sparkcc.py,$PROJECT_ROOT/tgrag/cc-scripts/wat_extract_links.py,$PROJECT_ROOT/tgrag/cc-scripts/json_importer.py" \
   "$PROJECT_ROOT/tgrag/cc-scripts/hostlinks_to_graph.py" \
   "$PROJECT_ROOT/bash_scripts/spark-warehouse/wat_output_table" \
