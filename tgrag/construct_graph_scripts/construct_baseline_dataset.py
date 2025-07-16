6dc4accbb6587731363c4f3c9173ebf1aa542564
ekmpa <emmakndp@gmail.com>
2025-07-16 12:42:17 -0400

    re-organize merging scripts

--- tgrag/construct_graph_scripts/construct_baseline_dataset.py
+++ tgrag/construct_graph_scripts/construct_baseline_dataset.py
@@ -1,12 +1,14 @@
 import argparse
 import logging
 
-from tgrag.utils.logger import setup_logging
-from tgrag.utils.merge_dqr_ratings_trie_filter import (
+from tgrag.construct_graph_scripts.merge_dqr_ratings_trie_filter import (
     merge_dqr_to_node_parallel,
 )
+from tgrag.construct_graph_scripts.txt_to_csv_sqlite import (
+    merge_vertices_rank_centrality,
+)
+from tgrag.utils.logger import setup_logging
 from tgrag.utils.path import get_root_dir
-from tgrag.utils.txt_to_csv_sqlite import merge_vertices_rank_centrality
 
 parser = argparse.ArgumentParser(
     description='Baseline Dataset Construction',
