1c3c1fd1df790b166913937e9f9df56914927bec
sebastian9991 <sebastian.sabry1@gmail.com>
2025-07-17 22:55:28 -0400

    update: importance functions for node csv file

--- tgrag/utils/prob.py
+++ tgrag/utils/prob.py
@@ -17,3 +17,14 @@ def get_importance(pr_src: str, hc_src: str, pr_dst: str, hc_dst: str) -> float:
     dst_importance = abs(float(pr_dst)) * abs(float(hc_dst))
     importance = (src_importance + dst_importance) / 2
     return importance
+
+
+def get_importance_probability_node(
+    pr_val: str, hc_val: str, average_importance: float
+) -> float:
+    importance = get_importance_node(pr_val, hc_val)
+    return sigmoid(importance / average_importance)
+
+
+def get_importance_node(pr_val: str, hc_val: str) -> float:
+    return abs(float(pr_val) * float(hc_val))
