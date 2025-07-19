7a7ee17998db4a71b7e5da7f7f496b17adc29c4b
sebastian9991 <sebastian.sabry1@gmail.com>
2025-07-19 14:57:29 -0400

    update: making baseline construct script more general

--- tgrag/construct_graph_scripts/construct_baseline_dataset.py
+++ tgrag/construct_graph_scripts/construct_baseline_dataset.py
@@ -31,24 +31,24 @@ parser.add_argument(
 def construct_baseline() -> None:
     args = parser.parse_args()
     root = get_root_dir()
-    folder_path = f'{root}/data/baseline/'
+    dqr_path = f'{root}/data/dqr/domain_pc1.csv'
+    folder_path = f'{args.baseline_folder}'
     setup_logging(args.log_file)
     logging.info('***Merging Rank/Centrality with vertices.txt***')
     logging.info('***Converting txt to csv ***')
     merge_vertices_rank_centrality(folder_path)
     logging.info('***Merge DQR gold-standard labels***')
-    node_path = f'{root}/data/baseline/temporal_nodes.csv'
-    dqr_path = f'{root}/data/dqr/domain_pc1.csv'
-    output_path = f'{root}/data/baseline/temporal_nodes_scored.csv'
-    edges_path = f'{root}/data/baseline/temporal_edges.csv'
-    filter_edges_output_path = f'{root}/data/baseline/temporal_edges_filtered.csv'
+    node_path = f'{args.baseline_folder}/cc_baseline_nodes.csv'
+    output_path = f'{args.baseline_folder}/cc_baseline_nodes_scored.csv'
+    edges_path = f'{args.baseline_folder}/cc_baseline_edges.csv'
+    filter_edges_output_path = f'{args.baseline_folder}/cc-baseline_edges_filtered.csv'
     workers = 16
     merge_dqr_to_node_parallel(
-        node_path,
-        dqr_path,
-        edges_path,
-        output_path,
-        filter_edges_output_path,
+        node_path=node_path,
+        dqr_path=dqr_path,
+        edges_path=edges_path,
+        output_path=output_path,
+        filtered_edges_output_path=filter_edges_output_path,
         workers=workers,
         chunk_size=100_000,
     )
