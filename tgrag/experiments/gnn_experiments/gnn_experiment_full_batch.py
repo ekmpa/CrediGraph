5a2f2a0e77082037e715520eb5ec24ab8e27becd
sebastian9991 <sebastian.sabry1@gmail.com>
2025-07-20 18:07:26 -0400

    adding full batch version of gnn experiment

---
+++ tgrag/experiments/gnn_experiments/gnn_experiment_full_batch.py
@@ -0,0 +1,154 @@
+import logging
+import pickle
+from typing import Dict, List, Tuple, Type, cast
+
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+
+from tgrag.dataset.temporal_dataset import TemporalDataset
+from tgrag.encoders.encoder import Encoder
+from tgrag.encoders.log_encoding import LogEncoder
+from tgrag.encoders.rni_encoding import RNIEncoder
+from tgrag.gnn.GAT import GAT
+from tgrag.gnn.gCon import GCN
+from tgrag.gnn.SAGE import SAGE
+from tgrag.utils.args import DataArguments, ModelArguments
+from tgrag.utils.logger import Logger
+from tgrag.utils.path import get_root_dir
+from tgrag.utils.plot import plot_avg_rmse_loss
+
+MODEL_CLASSES: Dict[str, Type[torch.nn.Module]] = {
+    'GCN': GCN,
+    'GAT': GAT,
+    'SAGE': SAGE,
+}
+
+ENCODER_CLASSES: Dict[str, Encoder] = {
+    'RNI': RNIEncoder(),
+    'LOG': LogEncoder(),
+}
+
+
+def save_loss_results(
+    loss_tuple_run: List[List[Tuple[float, float, float]]],
+    model_name: str,
+    encoder_name: str,
+) -> None:
+    root = get_root_dir()
+    save_dir = root / 'results' / 'logs' / model_name / encoder_name
+    save_dir.mkdir(parents=True, exist_ok=True)
+    save_path = save_dir / 'loss_tuple_run.pkl'
+    with open(save_path, 'wb') as f:
+        pickle.dump(loss_tuple_run, f)
+
+
+def train(
+    model: torch.nn.Module,
+    data: TemporalDataset,
+    train_idx: torch.Tensor,
+    optimizer: torch.optim.Adam,
+) -> float:
+    model.train()
+    optimizer.zero_grad()
+    out = model(data.x, data.adj_t)[train_idx]
+    loss = F.mse_loss(out.squeeze(), data.y.squeeze(1)[train_idx])
+    loss.backward()
+    optimizer.step()
+
+    return loss.item()
+
+
+@torch.no_grad()
+def evaluate(
+    model: torch.nn.Module,
+    data: TemporalDataset,
+    split_idx: Dict,
+) -> Tuple[float, float, float]:
+    model.eval()
+    out = model(data.x, data.edge_index)
+
+    y_true = data.y
+    y_pred = out
+
+    train_rmse = torch.sqrt(
+        F.mse_loss(y_pred[split_idx['train']], y_true[split_idx['train']])
+    ).item()
+    valid_rmse = torch.sqrt(
+        F.mse_loss(y_pred[split_idx['valid']], y_true[split_idx['valid']])
+    ).item()
+    test_rmse = torch.sqrt(
+        F.mse_loss(y_pred[split_idx['test']], y_true[split_idx['test']])
+    ).item()
+
+    return train_rmse, valid_rmse, test_rmse
+
+
+def run_gnn_baseline_full_batch(
+    data_arguments: DataArguments,
+    model_arguments: ModelArguments,
+) -> None:
+    logging.info('Running Full-Batch')
+    logging.info(
+        'Setting up training for task of: %s on model: %s',
+        data_arguments.task_name,
+        model_arguments.model,
+    )
+    device = f'cuda:{model_arguments.device}' if torch.cuda.is_available() else 'cpu'
+    device = torch.device(device)
+
+    root_dir = get_root_dir()
+
+    model_class = MODEL_CLASSES[model_arguments.model]
+    encoder_class = ENCODER_CLASSES[model_arguments.encoder]
+    logging.info(
+        'Encoder: %s is used on column: %s',
+        model_arguments.encoder,
+        model_arguments.encoder_col,
+    )
+
+    encoding_dict: Dict[str, Encoder] = {model_arguments.encoder_col: encoder_class}
+
+    dataset = TemporalDataset(
+        root=f'{root_dir}/data/crawl-data/temporal',
+        node_file=cast(str, data_arguments.node_file),
+        edge_file=cast(str, data_arguments.edge_file),
+        encoding=encoding_dict,
+    )
+    data = dataset[0]
+    data.y = data.y.squeeze(1)
+    split_idx = dataset.get_idx_split()
+    train_idx = split_idx['train'].to(device)
+
+    logging.info(f'Training set size: {split_idx["train"].size()}')
+    logging.info(f'Validation set size: {split_idx["valid"].size()}')
+    logging.info(f'Testing set size: {split_idx["test"].size()}')
+
+    model = model_class(
+        data.num_features,
+        model_arguments.hidden_channels,
+        1,
+        model_arguments.num_layers,
+        model_arguments.dropout,
+        cached=False,
+    ).to(device)
+
+    logger = Logger(model_arguments.runs)
+
+    loss_tuple_run: List[List[Tuple[float, float, float]]] = []
+    logging.info('*** Training ***')
+    for run in tqdm(range(model_arguments.runs), desc='Runs'):
+        model.reset_parameters()
+        optimizer = torch.optim.Adam(model.parameters(), lr=model_arguments.lr)
+        loss_tuple_epoch: List[Tuple[float, float, float]] = []
+        for _ in tqdm(range(1, 1 + model_arguments.epochs), desc='Epochs'):
+            train(model, data, train_idx, optimizer)
+            result = evaluate(model, data, split_idx)
+            loss_tuple_epoch.append(result)
+            logger.add_result(run, result)
+
+        loss_tuple_run.append(loss_tuple_epoch)
+
+    logging.info(logger.get_statistics())
+    plot_avg_rmse_loss(loss_tuple_run, model_arguments.model, model_arguments.encoder)
+    save_loss_results(loss_tuple_run, model_arguments.model, model_arguments.encoder)
