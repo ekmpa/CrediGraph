8eb496595100be4bed2d755df3e5ccb845475092
sebastian9991 <sebastian.sabry1@gmail.com>
2025-07-21 23:51:07 -0400

    adding norm encoder

--- tgrag/encoders/norm_encoding.py
+++ tgrag/encoders/norm_encoding.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch import Tensor
 
@@ -5,12 +6,12 @@ from tgrag.encoders.encoder import Encoder
 
 
 class NormEncoder(Encoder):
-    def __init__(self, scale: float | None = None):
-        self.scale = scale
+    def __call__(self, input: np.ndarray) -> Tensor:
+        if input.ndim == 1:
+            input = input.reshape(-1, 1)
 
-    def __call__(self, input: float) -> Tensor:
-        x = torch.as_tensor(input, dtype=torch.float32).unsqueeze(-1)
-        x = torch.log1p(x)
-        if self.scale:
-            x = x * self.scale
-        return x
+        mean = input.mean(axis=0, keepdims=True)
+        std = input.std(axis=0, keepdims=True) + 1e-8
+
+        normalized = (input - mean) / std
+        return torch.tensor(normalized)
