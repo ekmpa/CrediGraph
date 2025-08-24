e1f340dc42fe0980ac4bbffaf2dfda7582c03d15
hussein <hussien.shahata@gmail.com>
2025-08-24 10:18:10 -0400

    update readme

--- bash_scripts/run_in_batches_url.sh
+++ bash_scripts/run_in_batches_url.sh
@@ -2,7 +2,7 @@
 set -e
 for ((i=0; i<90001; i+=50)); do
     echo "#########################################################################################"
-    echo "/end-to-end.sh  CC-Crawls/Oct2024.txt $i $((i+49)) [wet]"
+    echo "/end-to-end-url.sh  CC-Crawls/Oct2024.txt $i $((i+49)) [wet]"
     ./end-to-end-url.sh  CC-Crawls/Oct2024.txt $i $((i+49)) [wet]
     rm -r ../data/crawl-data/CC-MAIN-2024-42/segments
 done
