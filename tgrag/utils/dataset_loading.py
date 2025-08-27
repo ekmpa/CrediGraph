97aa4a9c311eab727301819c2ce18498a5478fb0
sebastian9991 <sebastian.sabry1@gmail.com>
2025-08-26 20:44:43 -0400

    update: returning removed code for dataset loading

---
+++ tgrag/utils/dataset_loading.py
@@ -0,0 +1,68 @@
+from typing import Dict, Tuple
+
+import pandas as pd
+import torch
+from torch import Tensor
+from tqdm import tqdm
+
+
+def load_node_csv(
+    path: str, index_col: int, encoders: Dict | None = None, chunk_size: int = 500_000
+) -> Tuple[Tensor | None, Dict]:
+    dfs = []
+    total_rows = sum(1 for _ in open(path)) - 1
+    with pd.read_csv(path, index_col=index_col, chunksize=chunk_size) as reader:
+        for chunk in tqdm(
+            reader, total=total_rows // chunk_size + 1, desc='Reading node CSV'
+        ):
+            dfs.append(chunk)
+
+    df = pd.concat(dfs, axis=0)
+    mapping = {
+        index: i for i, index in tqdm(enumerate(df.index.unique()), desc='Indexing')
+    }
+
+    x = None
+    if encoders is not None:
+        xs = []
+        for key, encoder in encoders.items():
+            if key in df.columns:
+                xs.append(encoder(df[key].values))
+            else:
+                # Global encoder (In our case the RNIEncoder)
+                xs.append(encoder(df.shape[0]))
+
+        x = torch.cat(xs, dim=-1)
+
+    return x, mapping
+
+
+def load_edge_csv(
+    path: str,
+    src_index_col: str,
+    dst_index_col: str,
+    mapping: Dict,
+    encoders: Dict | None = None,
+) -> Tuple[torch.Tensor, torch.Tensor | None]:
+    usecols = [src_index_col, dst_index_col]
+    if encoders is not None:
+        usecols += [col for col in encoders if col not in usecols]
+
+    df = pd.read_csv(path, usecols=usecols)
+
+    src = torch.tensor([mapping[s] for s in df[src_index_col]], dtype=torch.long)
+    dst = torch.tensor([mapping[d] for d in df[dst_index_col]], dtype=torch.long)
+    edge_index = torch.stack([src, dst], dim=0)  # Shape: [2, num_edges]
+
+    edge_attr = None
+    if encoders is not None:
+        edge_attrs = []
+        for key, encoder in encoders.items():
+            if key in df.columns:
+                edge_attrs.append(encoder(df[key]))
+            else:
+                edge_attrs.append(encoder(len(df)))
+
+        edge_attr = torch.cat(edge_attrs, dim=-1)
+
+    return edge_index, edge_attr
