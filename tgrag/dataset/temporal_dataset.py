a296aaa1f358208ab1525f9ef74ad1b20a075f57
sebastian9991 <sebastian.sabry1@gmail.com>
2025-10-01 21:28:14 -0400

    update: mapping accesible via field

--- tgrag/dataset/temporal_dataset.py
+++ tgrag/dataset/temporal_dataset.py
@@ -88,6 +88,7 @@ class TemporalDataset(InMemoryDataset):
             index_col=0,
             encoders=self.encoding,
         )
+        self.mapping = mapping
         logging.info('***Feature Matrix Done***')
 
         if x_full is None:
