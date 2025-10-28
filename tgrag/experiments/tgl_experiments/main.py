b33bb70dff959b96bd681ea32560e8787f7baaa9
sebastian9991 <sebastian.sabry1@gmail.com>
2025-10-28 18:10:52 -0400

    cardinality change

--- tgrag/experiments/tgl_experiments/main.py
+++ tgrag/experiments/tgl_experiments/main.py
@@ -33,7 +33,7 @@ def initialize_graph_db(
     conn.execute(
         'CREATE NODE TABLE node(domain STRING, ts INT64, PRIMARY KEY(domain));'
     )
-    conn.execute('CREATE REL TABLE edge(FROM node TO node, MANY_MANY);')
+    conn.execute('CREATE REL TABLE edge(FROM node TO node, ts INT64, MANY_MANY);')
     conn.execute(f'COPY node FROM "{nodes_csv}" (HEADER=true);')
     conn.execute(f'COPY edge FROM "{edges_csv}" (HEADER=true);')
     logging.info('Graph database initialized')
