be0eb05f012a72a92219ea7740cb9eada8c5b9d3
sebastian9991 <sebastian.sabry1@gmail.com>
2025-10-28 17:06:36 -0400

    construct id w/ tqdm, args with new paramater

--- tgrag/experiments/tgl_experiments/main.py
+++ tgrag/experiments/tgl_experiments/main.py
@@ -7,6 +7,7 @@ from typing import cast
 import kuzu
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 from tgrag.utils.args import parse_args
 from tgrag.utils.logger import setup_logging
@@ -25,18 +26,36 @@ parser.add_argument(
 )
 
 
-def construct_id(node_path: Path, edge_path: Path) -> None:
+def construct_id(node_path: Path, edge_path: Path, chunksize: int = 5_000_000) -> None:
+    node_out = node_path.parent / 'nodes_with_id.csv'
+    edge_out = edge_path.parent / 'edges_with_id.csv'
+
+    if node_out.exists() and edge_out.exists():
+        logging.info(f'Found existing {node_out.name} and {edge_out.name}, skipping.')
+        return
+
+    logging.info('Reading nodes and assigning IDs...')
     verts = pd.read_csv(node_path)
     verts['id'] = np.arange(len(verts))
     id_map = dict(zip(verts['domain'], verts['id']))
 
-    edges = pd.read_csv(edge_path)
-    edges['src_id'] = edges['src'].map(id_map)
-    edges['dst_id'] = edges['dst'].map(id_map)
-    edges[['src_id', 'dst_id']].to_csv(
-        edge_path / 'edge_with_id.csv', index=False, header=['src', 'dst']
-    )
-    verts[['id']].to_csv(node_path / 'nodes_with_id.csv', index=False)
+    logging.info(f'Writing node IDs to: {node_out}')
+    verts[['id']].to_csv(node_out, index=False)
+
+    logging.info('Mapping and writing edges with IDs...')
+    with open(edge_out, 'w') as fout:
+        fout.write('src,dst\n')
+        total_lines = sum(1 for _ in open(edge_path)) - 1  # minus header
+        reader = pd.read_csv(edge_path, chunksize=chunksize)
+
+        for chunk in tqdm(
+            reader, total=total_lines // chunksize + 1, desc='Processing edges'
+        ):
+            chunk['src_id'] = chunk['src'].map(id_map)
+            chunk['dst_id'] = chunk['dst'].map(id_map)
+            chunk[['src_id', 'dst_id']].to_csv(fout, index=False, header=False)
+
+    logging.info(f'Completed writing: {edge_out}')
 
 
 def main() -> None:
@@ -55,13 +74,15 @@ def main() -> None:
     )
 
     logging.info('Connecting graph storage backend')
-    db = kuzu.Database('graphdb')
+    db_path = scratch / cast(str, meta_args.database_folder) / 'graphdb'
+    db = kuzu.Database(db_path)
     conn = kuzu.Connection(db, num_threads=cpu_count())
 
     conn.execute('CREATE NODE TABLE node(id INT64, PRIMARY KEY(id));')
     conn.execute('CREATE REL TABLE edge(FROM node TO node, MANY_MANY);')
-    conn.execute('COPY node FROM "nodes.csv" (id);')
-    conn.execute('COPY edge FROM "edge_index.csv" (src, dst);')
+    conn.execute(f'COPY node FROM "{db_path}/nodes_with_id.csv" (id);')
+    conn.execute(f'COPY edge FROM "{db_path}/edges_with_id.csv" (src, dst);')
+    logging.info('Graph database initialized')
 
     for experiment, experiment_arg in experiment_args.exp_args.items():
         logging.info(f'\n**Running**: {experiment}')
