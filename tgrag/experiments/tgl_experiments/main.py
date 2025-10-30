5412ce99978867840801b3a046c0ead8fb8a73f7
sebastian9991 <sebastian.sabry1@gmail.com>
2025-10-30 17:06:03 -0400

    removing graphdb in absolute path

--- tgrag/experiments/tgl_experiments/main.py
+++ tgrag/experiments/tgl_experiments/main.py
@@ -54,7 +54,7 @@ def initialize_graph_db(
 
     if db_path.exists():
         logging.info(f'Existing database found at {db_path}, skipping initalization.')
-        db = kuzu.Database(db_path, buffer_pool_size=buffer * 1024**3)
+        db = kuzu.Database(db_path / 'graphdb', buffer_pool_size=buffer * 1024**3)
         conn = kuzu.Connection(db, num_threads=cpu_count())
         return db, conn
 
@@ -93,7 +93,7 @@ def main() -> None:
     )
 
     db, conn = initialize_graph_db(
-        db_path=db_path / 'graphdb', nodes_csv=node_path, edges_csv=edge_path
+        db_path=db_path, nodes_csv=node_path, edges_csv=edge_path
     )
 
     node_df = conn.execute(
