b8f6f371daa172738e66fe58d7eb93183049ef46
sebastian9991 <sebastian.sabry1@gmail.com>
2025-11-08 15:00:18 -0500

    update: adding tests w/ INTEGER id

---
+++ test/test_relational_database/test_sqlite_relational_with_nid.py
@@ -0,0 +1,118 @@
+import sqlite3
+from pathlib import Path
+
+import numpy as np
+import pytest
+import torch
+from torch_geometric.data.feature_store import TensorAttr
+
+from tgrag.dataset.torch_geometric_feature_store import SQLiteFeatureStore
+
+
+@pytest.fixture
+def db_path(tmp_path: Path):
+    db_file = tmp_path / "test_graph.db"
+
+    record = [
+        {"domain": 0, "ts": 20241111, "y": -1.0, "x": [0.1, 0.2, 0.3]},
+        {"domain": 1, "ts": 20241111, "y": -1.0, "x": [0.1, -0.04, 0.32]},
+    ]
+    con = sqlite3.connect(db_file)
+    con.execute(
+        "CREATE TABLE domain(id INTEGER PRIMARY KEY, ts INTEGER, x BLOB, y REAL)"
+    )
+    for r in record:
+        x = np.array(r["x"], dtype=np.float32).tobytes()
+
+        con.execute(
+            "INSERT INTO domain VALUES (?, ?, ?, ?)",
+            (int(r["domain"]), int(r["ts"]), x, float(r["y"])),
+        )
+    con.commit()
+    con.close()
+
+    return db_file
+
+
+def test_get_all_tensor_attrs(db_path) -> None:
+    feature_store = SQLiteFeatureStore(db_path=db_path)
+
+    attrs = feature_store.get_all_tensor_attrs()
+    assert len(attrs) == 4
+    print(attrs)
+
+
+def test_get_tensor_size(db_path) -> None:
+    t = TensorAttr(group_name="domain", attr_name="x")
+    feature_store = SQLiteFeatureStore(db_path=db_path)
+
+    n = feature_store._get_tensor_size(attr=t)
+    assert n == (2, 3)
+
+    t = TensorAttr(group_name="domain", attr_name="ts")
+    n = feature_store._get_tensor_size(attr=t)
+    assert n == (2, 1)
+
+    t = TensorAttr(group_name="domain", attr_name="y")
+    n = feature_store._get_tensor_size(attr=t)
+    assert n == (2, 1)
+
+    t = TensorAttr(group_name="domain", attr_name="id")
+    n = feature_store._get_tensor_size(attr=t)
+    assert n == (2, 1)
+
+
+def test_get_table_columns(db_path) -> None:
+    feature_store = SQLiteFeatureStore(db_path=db_path)
+    columns = feature_store._get_table_columns(table="domain")
+    assert len(columns) == 4
+    assert columns[0] == "id"
+    assert columns[1] == "ts"
+    assert columns[2] == "x"
+    assert columns[3] == "y"
+
+
+def test_get_tensor_single_index(db_path) -> None:
+    feature_store = SQLiteFeatureStore(db_path=db_path)
+    x_features_0 = feature_store["domain", "x", 0]
+    assert torch.equal(x_features_0, torch.tensor([[0.1, 0.2, 0.3]]))
+
+    x_features_1 = feature_store["domain", "x", 1]
+    assert torch.equal(x_features_1, torch.tensor([[0.1, -0.04, 0.32]]))
+
+
+def test_get_tensor_slice(db_path) -> None:
+    feature_store = SQLiteFeatureStore(db_path=db_path)
+    x_features = feature_store["domain", "x", 0:2]
+    assert torch.equal(x_features, torch.tensor([[0.1, 0.2, 0.3], [0.1, -0.04, 0.32]]))
+
+
+def test_get_tensor_slice_ts(db_path) -> None:
+    feature_store = SQLiteFeatureStore(db_path=db_path)
+    x_features = feature_store["domain", "ts", 0:2]
+    assert torch.equal(x_features, torch.tensor([20241111, 20241111]))
+
+
+def test_get_tensor_list(db_path) -> None:
+    feature_store = SQLiteFeatureStore(db_path=db_path)
+    x_features = feature_store["domain", "x", [0, 1]]
+    assert torch.equal(x_features, torch.tensor([[0.1, 0.2, 0.3], [0.1, -0.04, 0.32]]))
+
+
+def test_get_tensor_list_id(db_path) -> None:
+    feature_store = SQLiteFeatureStore(db_path=db_path)
+    x_features = feature_store["domain", "id", [0, 1]]
+    assert torch.equal(x_features, torch.tensor([0, 1]))
+
+
+def test_get_tensor_list_y(db_path) -> None:
+    feature_store = SQLiteFeatureStore(db_path=db_path)
+    x_features = feature_store["domain", "y", [0, 1]]
+    assert torch.equal(x_features, torch.tensor([-1.0, -1.0]))
+
+
+def test_get_all_tables(db_path) -> None:
+    feature_store = SQLiteFeatureStore(db_path=db_path)
+
+    tables = feature_store._get_all_tables()
+    assert tables == ["domain"]
