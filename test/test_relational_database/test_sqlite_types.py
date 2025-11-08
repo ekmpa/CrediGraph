3f93a0e8a9badf5cf906dd865336ba483f7c8aa7
sebastian9991 <sebastian.sabry1@gmail.com>
2025-11-08 15:01:15 -0500

    update: adding tests on sqlite3 type insertion

---
+++ test/test_relational_database/test_sqlite_types.py
@@ -0,0 +1,32 @@
+import json
+import sqlite3
+
+import numpy as np
+import pytest
+
+
+def test_numpy_type_insert():
+    record = {"domain": "0.0", "ts": 20241111, "y": -1.0, "x": [0.1, 0.2, 0.3]}
+    x = np.array(record["x"], dtype=np.float32).tobytes()
+
+    con = sqlite3.connect(":memory:")
+    con.execute(
+        "CREATE TABLE domain(name TEXT PRIMARY KEY, ts INTEGER, x BLOB, y REAL)"
+    )
+    con.execute(
+        "INSERT INTO domain VALUES (?, ?, ?, ?)",
+        (str(record["domain"]), int(record["ts"]), x, float(record["y"])),
+    )
+
+def test_numpy_type_insert_float():
+    record = {"domain": "www.google.com", "ts": 20241111, "y": 0.5, "x": [0.1, 0.2, 0.3]}
+    x = np.array(record["x"], dtype=np.float32).tobytes()
+
+    con = sqlite3.connect(":memory:")
+    con.execute(
+        "CREATE TABLE domain(name TEXT PRIMARY KEY, ts INTEGER, x BLOB, y REAL)"
+    )
+    con.execute(
+        "INSERT INTO domain VALUES (?, ?, ?, ?)",
+        (str(record["domain"]), int(record["ts"]), x, float(record["y"])),
+    )
