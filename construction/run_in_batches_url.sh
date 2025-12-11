7288404f6fbb9a8b44856688434db7168e15220e
ekmpa <emmakndp@gmail.com>
2025-12-11 10:35:37 -0500

    cleaned

--- construction/run_in_batches_url.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-for ((i=0; i<90001; i+=50)); do
-    echo "#########################################################################################"
-    echo "/end-to-end-url.sh  CC-Crawls/Oct2024.txt $i $((i+49)) [wet]"
-    ./end-to-end-url.sh  CC-Crawls/Oct2024.txt $i $((i+49)) [wet]
-    rm -r ../data/crawl-data/CC-MAIN-2024-42/segments
-done
