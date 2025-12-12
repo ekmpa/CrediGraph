7445ffedd9ac34db6debb705a5b8dc70966813df
sebastian9991 <sebastian.sabry1@gmail.com>
2025-12-11 19:43:09 -0500

    reverted scratch location

--- tgrag/dataset/temporal_dataset.py
+++ tgrag/dataset/temporal_dataset.py
@@ -115,7 +115,9 @@ class TemporalDataset(InMemoryDataset):
 
         labeled_mask = (score != -1.0)[:, 0]
 
-        labeled_idx = torch.nonzero(torch.tensor(labeled_mask), as_tuple=True)[0]
+        labeled_idx = torch.nonzero(
+            torch.tensor(labeled_mask.detach().clone()), as_tuple=True
+        )[0]
         labeled_scores = score[:, 0][labeled_idx].squeeze().numpy()
 
         if labeled_scores.size == 0:
