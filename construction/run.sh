675b2103e132483e49e11b7c952a133033f794de
ekmpa <emmakndp@gmail.com>
2025-12-13 23:30:50 -0500

    added preemption practice from mila documentation

--- construction/run.sh
+++ construction/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --partition=long  #unkillable #main #long
-#SBATCH --output=logs/January-pipeline-%j.out
-#SBATCH --error=logs/January-pipeline-%j.err
+#SBATCH --output=logs/March-pipeline-%j.out
+#SBATCH --error=logs/March-pipeline-%j.err
 #SBATCH --cpus-per-task=8                     # Ask for 4 CPUs
-#SBATCH --gres=gpu:rtx8000:1                  # Ask for 1 titan xp
+#SBATCH --gres=gpu:rtx8000:1   
 #SBATCH --mem=256G
 #SBATCH --time=168:00:00                       # The job will run for 1 day
-#SBATCH --job-name=January-pipeline
+#SBATCH --job-name=March-pipeline
 
 export HOME="/home/mila/k/kondrupe"
 export PATH="$HOME/bin:$PATH"
@@ -14,6 +14,15 @@ module load python/3.10
 source ~/CGfullgraph/.venv/bin/activate
 export JAVA_HOME="$HOME/java/jdk-17"
 
-bash pipeline.sh 'January 2025' 'January 2025'
+exit_script() {
+    echo "Preemption signal, saving myself"
+    trap - SIGTERM # clear the trap
+    # Optional: sends SIGTERM to child/sub processes
+    kill -- -$$
+}
+
+trap exit_script SIGTERM
+
+bash pipeline.sh 'March 2025' 'March 2025'
 
 # if memory: try lock on aggregate or batch
