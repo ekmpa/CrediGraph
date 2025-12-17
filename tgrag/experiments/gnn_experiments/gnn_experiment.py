11d2088221bcf77776ce71550e0df68508120fad
sebastian9991 <sebastian.sabry1@gmail.com>
2025-12-17 00:09:48 -0500

    logging cls, shape

--- tgrag/experiments/gnn_experiments/gnn_experiment.py
+++ tgrag/experiments/gnn_experiments/gnn_experiment.py
@@ -97,10 +97,12 @@ def train_(
 
         regression_indices = torch.nonzero(targets != -1.0)
         cls_indices = torch.nonzero(targets_cls != -1.0)
+        logging.info(f'indices shape: {cls_indices.shape}')
 
         targets_cls = F.one_hot(
             targets_cls[cls_indices].long(), num_classes=2
         ).squeeze()
+        logging.info(f'target_cls shape: {targets_cls.shape}')
         logging.info(f'targets_cls: {targets_cls[cls_indices]}')
         if regression_indices.numel() and cls_indices.numel():
             loss_reg = F.l1_loss(preds[regression_indices], targets[regression_indices])
