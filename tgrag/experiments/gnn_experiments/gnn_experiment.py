fe87eff03fa3881fca8447d8b6d67cc874f13e80
sebastian9991 <sebastian.sabry1@gmail.com>
2025-12-16 23:03:42 -0500

    logging classes

--- tgrag/experiments/gnn_experiments/gnn_experiment.py
+++ tgrag/experiments/gnn_experiments/gnn_experiment.py
@@ -98,6 +98,7 @@ def train_(
 
         regression_indices = torch.nonzero(targets != -1.0)
         cls_indices = torch.nonzero(targets_cls != -1.0)
+        logging.info(f'cls_indices: {cls_indices}')
 
         if regression_indices.numel() and cls_indices.numel():
             loss_reg = F.l1_loss(preds[regression_indices], targets[regression_indices])
